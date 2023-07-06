becec335de8158db1c2603d29de98d1bf613f023
Mashyrano <106875049+Mashyrano@users.noreply.github.com>
2023-07-06 01:11:14 -0700

    the game has direction

--- solve.js
+++ solve.js
@@ -1,3 +1,6 @@
+// color pool
+colorPool = ['blue', 'green', 'yellow', 'red'];
+
 // load sounds
 let blueBeat = new Audio('/sounds/blue.mp3');
 let redBeat = new Audio('/sounds/red.mp3');
@@ -10,17 +13,11 @@ document.querySelector("#blue").addEventListener("click", function () {checkInpu
 document.querySelector("#green").addEventListener("click", function () {checkInput('green');});
 document.querySelector("#red").addEventListener("click", function () {checkInput('red');});
 document.querySelector("#yellow").addEventListener("click", function () {checkInput('yellow');});
-/*
-Define Global Array
-    blue
-    red
-    green
-    yellow
-*/
-var arr = Array();
+
+var colorArray = Array();
 
 // Define pointer variable to keep track of user progress
-var pointer;
+var pointer=0;
 
 
 function checkInput(color){
@@ -35,21 +32,41 @@ function checkInput(color){
 
     //document.getElementById("level-title").style.display = "none";
 
-    // check if color is correct
-    var correct = true;
-    if (correct) {
-        // add sound
-        if (color == 'green'){
-            greenBeat.play();
+    // check if it is first click
+    if (colorArray.length == 0){
+        // record 
+        playSound(color)
+        colorArray.push(color);
+        addRandom();
+        playPattern();
+    }
+    else {
+        // check if color is correct
+        var correct;
+
+        if (colorArray[pointer] == color){
+            correct = true;
         }
-        else if (color == 'blue'){
-            blueBeat.play();
-        }
-        else if (color == 'red'){
-            redBeat.play();
+
+        if (correct) {
+            // add sound
+            playSound(color);
+            if (pointer == (colorArray.length -1)){
+                pointer = 0;
+                addRandom();
+                playPattern();
+            }
+            else{
+                pointer++;
+            }
+
         }
         else {
-            yellowBeat.play();
+            // end game and reset variable
+            pointer = 0;
+            colorArray = [];
+            wrongBeat.play();
+            // display Game Over
         }
     }
 
@@ -57,10 +74,41 @@ function checkInput(color){
 
 function addRandom(){
     // generate some random value and add it to array
+    var i = Math.floor(Math.random() * 4);
+    colorArray.push(colorPool[i]);    
 }
 
-function playPattern(){
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function playPattern(){
     // light the colors and play the sound of the color
+    await sleep(1000);
+    var color;
+    for (let i = 0; i < colorArray.length; i++) {
+        color = colorArray[i];
+        document.getElementById(color).style.opacity = "0.2";
+        playSound(color);
+        await sleep(500);
+        document.getElementById(color).style.opacity = "0.8";
+    }
+    }
+
+function playSound(color){
+    if (color == 'green'){
+        greenBeat.play();
+    }
+    else if (color == 'blue'){
+        blueBeat.play();
+    }
+    else if (color == 'red'){
+        redBeat.play();
+    }
+    else {
+        yellowBeat.play();
+    }
 }
 
 function changeOpacity(color){
@@ -70,3 +118,4 @@ function changeOpacity(color){
 function returnOpacity(color){
     color.style.opacity = "0.8";
 }
+
